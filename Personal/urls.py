572f5958bcd7de9c2993afa1534fb936971acdf1
Umar <51199173+umarjoniy@users.noreply.github.com>
2024-05-31 14:29:55 +0200

    Update urls.py

--- Personal/urls.py
+++ Personal/urls.py
@@ -33,4 +33,4 @@ urlpatterns = [
 ]
 # Is not suitable for a good project
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-urlpatterns += static(settings.STATIC_URL, document_root=settings.STATICFILES_DIRS)
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
